617cb48b3e155e5030dce08b071b490e303cb75f
FrogB <116889215+FrogB@users.noreply.github.com>
2023-04-20 11:24:56 +0800

    fixed cube tweens + new deceit

--- assets/preload/data/empyrean/script that makes the tunnel spin.lua
+++ assets/preload/data/empyrean/script that makes the tunnel spin.lua
@@ -1,23 +1,13 @@
-local basething = -100
-local someamountidfk = 25
-local TIME = 5
+local dalapsed = 0.0;
 function onStartCountdown()
 	doTweenAngle('the funny red tunnel spin', 'redTunnel', 14040, 8000, 'quartzInOut'); -- (OLD) Antagonism: First 11 Minutes - Bambi's Purgatory OST
 end
 
 function onCreatePost()
-    runTimer('fuckinUP', 0.001)
     setProperty('gf.alpha',0)
 end
-    
-function onTimerCompleted(tag, loops, loopsLeft)
-    if tag == 'fuckinUP' then
-        doTweenY('fuckinshit1', 'cubes', basething - someamountidfk, TIME, 'quartInOut')
-        doTweenY('fuckinshit2', 'cubes', basething - someamountidfk, TIME, 'quartInOut')
-        runTimer('fuckinDOWN', TIME)
-    elseif tag == 'fuckinDOWN' then
-        doTweenY('fuckinshit1', 'cubes', basething + someamountidfk, TIME, 'quartInOut')
-        doTweenY('fuckinshit2', 'cubes', basething + someamountidfk, TIME, 'quartInOut')
-        runTimer('fuckinUP', TIME)
-    end
+
+function onUpdatePost(elapsed)
+    dalapsed = dalapsed + elapsed;
+    setProperty('cubes.y', -165 + 100 * math.sin((dalapsed - 10) * 0.5));
 end
